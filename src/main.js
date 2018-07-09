0ee29993424f29b3a91df841b49f41d1dd329025
JasonLiu001 <wangliu@isoftstone.com>
2018-07-09 11:44:38 +0800

    解决导入vuetify.min.css出错问题

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router/index'
 import store from './vuex/store'
+
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+// index.js or main.js
+// import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+// index.js or main.js
+// import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
 // 支持的字体预设 https://github.com/vuetifyjs/vuetify/blob/dev/src/components/Vuetify/mixins/icons.js
 Vue.use(Vuetify)
